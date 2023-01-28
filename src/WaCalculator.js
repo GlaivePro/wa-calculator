729b969b634944f7713975bfc6f46e1d1c31fe6d
Džuris <juris@glaive.pro>
2023-01-28 15:49:56 +0200

    Add option interface

--- src/WaCalculator.js
+++ src/WaCalculator.js
@@ -1,3 +1,34 @@
-export function WaCalculator() {
-	return 1
+export class WaCalculator {
+	options = {
+		discipline: null,
+		gender: 'm',
+		electronicMeasurement: true,
+		venueType: 'outdoor',
+		edition: '2017',
+	}
+
+	constructor(options) {
+		this.setOptions(options)
+	}
+
+	setOptions(options) {
+		if ('discipline' in options)
+			this.options.discipline = options.discipline
+
+		if ('gender' in options)
+			this.options.gender = options.gender
+
+		if ('electronicMeasurement' in options)
+			this.options.electronicMeasurement = options.electronicMeasurement
+
+		if ('venueType' in options)
+			this.options.venueType = options.venueType
+
+		if ('edition' in options)
+			this.options.edition = options.edition
+	}
+
+	getOptions() {
+		return { ...this.options }
+	}
 }
--- tests/WaCalculator.test.js
+++ tests/WaCalculator.test.js
@@ -1,12 +1,40 @@
-import { describe, test, expect } from 'vitest'
+import { describe, test, it, expect } from 'vitest'
 import { WaCalculator } from '../index.js'
 
-test('Tests work', () => {
-	expect(WaCalculator()).toBe(1)
+describe('options', () => {
+	it('can be read', () => {
+		const discipline = 'test12'
+		const calc = new WaCalculator({discipline})
 
-	describe('options', () => {})
+		const options = calc.getOptions()
+		expect(options).toBeTypeOf('object')
+		expect(options).toHaveProperty('discipline')
+		expect(options).toHaveProperty('gender')
+		expect(options).toHaveProperty('electronicMeasurement')
+		expect(options).toHaveProperty('venueType')
+		expect(options).toHaveProperty('edition')
 
-	describe('keys', () => {})
+		expect(options.discipline).toBe(discipline)
+		expect(options.venueType).toBe('outdoor') // testing defaults
+	})
 
-	describe('calculations', () => {})
+	it('can be set', () => {
+		const disc1 = 'test1'
+		const calc = new WaCalculator({discipline: disc1})
+		const opt1 = calc.getOptions()
+		expect(opt1.discipline).toBe(disc1)
+		expect(opt1.venueType).toBe('outdoor') // testing defaults
+
+		const disc2 = 'test2'
+		calc.setOptions({discipline: disc2})
+		const opt2 = calc.getOptions()
+		expect(opt2.discipline).toBe(disc2)
+		expect(opt2.venueType).toBe('outdoor') // others should remain unchanged
+	})
 })
+
+// describe('coefficients', () => {})
+
+// describe('keys', () => {})
+
+// describe('calculations', () => {})
