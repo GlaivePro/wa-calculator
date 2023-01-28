177ff11c792d945ebb49e362bd7fbabd08b39ca2
Džuris <juris@glaive.pro>
2023-01-28 14:46:26 +0200

    Sketch test structure

--- tests/WaCalculator.test.js
+++ tests/WaCalculator.test.js
@@ -1,6 +1,12 @@
-import { test, expect } from 'vitest'
+import { describe, test, expect } from 'vitest'
 import { WaCalculator } from '../index.js'
 
 test('Tests work', () => {
 	expect(WaCalculator()).toBe(1)
+
+	describe('options', () => {})
+
+	describe('keys', () => {})
+
+	describe('calculations', () => {})
 })
