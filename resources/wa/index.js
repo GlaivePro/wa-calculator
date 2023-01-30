92d549ea89ec7e39c0d0a752da267484fde63e20
Džuris <juris@glaive.pro>
2023-01-30 20:59:42 +0200

    Improve compatibility with ESLint

--- resources/wa/index.js
+++ resources/wa/index.js
@@ -1,4 +1,10 @@
-import edition2017 from './2017.json' assert {type: 'json'}
+// https://github.com/eslint/eslint/discussions/15305
+// import edition2017 from './2017.json' assert {type: 'json'}
+
+// https://stackoverflow.com/a/73747606/2182900
+import { readFileSync } from 'fs'
+const loadJSON = (path) => JSON.parse(readFileSync(new URL(path, import.meta.url)))
+const edition2017 = loadJSON('./2017.json')
 
 export default {
 	2017: edition2017,
